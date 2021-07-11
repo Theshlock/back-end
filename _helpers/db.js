2e3861a4e3dd92ba2782975cf6debdfb188c9df4
theshlock <lockton.sam@gmail.com>
2021-07-12 07:10:07 +1000

    small changes for quiz collection WIP

--- _helpers/db.js
+++ _helpers/db.js
@@ -5,5 +5,6 @@ mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionO
 mongoose.Promise = global.Promise;
 
 module.exports = {
-    User: require('../users/user.model')
+    User: require('../users/user.model'),
+    Quiz: require('../quiz/quiz.model')
 };
