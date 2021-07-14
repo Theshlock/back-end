b87896722a09de5f946e5882843e774dd424fdbd
theshlock <lockton.sam@gmail.com>
2021-07-14 17:33:29 +1000

    fixed quiz put?

--- quiz/quiz.service.js
+++ quiz/quiz.service.js
@@ -45,7 +45,7 @@ async function create(quizParam, userId) {
         var item = {
             name : quizParam.items[i].item,
             numSuccess : 0,
-            icon: quizParam.items[i].name
+            icon: quizParam.items[i].emoji
         }
         itemlist.push(item) 
         // console.log(itemlist)
